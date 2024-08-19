5ecbca2a453506225597c01cbc8f05190b54cd1b
Luis1745 <luisfernando17450@gmail.com>
2024-08-18 21:27:01 -0600

    edit

--- components/Socials.js
+++ components/Socials.js
@@ -10,16 +10,16 @@ import {
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.youtube.com/@luisfernandoregalado6322'} className='hover:text-accent transition-all duration-300'>
         <RiYoutubeFill />
       </Link>
       <Link href={''} className='hover:text-fuchsia-600 transition-all duration-300'>
         <RiInstagramFill />
       </Link>
-      <Link href={''} className='hover:text-indigo-600 transition-all duration-300'>
+      <Link href={'https://www.facebook.com/profile.php?id=100016138113874&mibextid=ZbWKwL'} className='hover:text-indigo-600 transition-all duration-300'>
         <RiFacebookFill />
       </Link>
-      <Link href={''} className='hover:text-green-600 transition-all duration-300'>
+      <Link href={'https://wa.me/523171111096'} className='hover:text-green-600 transition-all duration-300'>
         <RiWhatsappFill />
       </Link>
       <Link href={''} className='hover:text-cyan-700 transition-all duration-300'>
