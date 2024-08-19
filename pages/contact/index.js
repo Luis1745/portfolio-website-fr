4de66b748a13e04793fd079209ca442d6bcd2aaa
Luis1745 <luisfernando17450@gmail.com>
2024-08-19 12:59:11 -0600

    update

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,13 +1,12 @@
-import Circles from '/components/Circles'
-import { BsArrowRightCircle } from 'react-icons/bs'
-import { motion } from 'framer-motion'
-import { fadeIn } from '../../variants'
+import Circles from '../components/Circles'; 
+import { BsArrowRightCircle } from 'react-icons/bs';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 
 const Contact = () => {
   return (
     <div className='h-full bg-primary/30'>
-      <div className='container mx-auto py-32 text-center xl:text-left flex
-    items-center justify-center h-full'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
         <div className='flex flex-col w-full max-w-[700px]'>
           <motion.h2
             variants={fadeIn('up', 0.2)}
@@ -16,14 +15,15 @@ const Contact = () => {
             exit='hidden'
             className='h2 text-center mb-12'
           >
-            Let’s <span className='text-accent'>connet.</span>
+            Let’s <span className='text-accent'>connect.</span>
           </motion.h2>
           <motion.form
             variants={fadeIn('up', 0.4)}
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='flex-1 flex flex-col gap-6 w-full mx-auto'>
+            className='flex-1 flex flex-col gap-6 w-full mx-auto'
+          >
             <div className='flex gap-x-6 w-full'>
               <input type='text' placeholder='name' className='input' />
               <input type='text' placeholder='email' className='input' />
@@ -31,9 +31,9 @@ const Contact = () => {
             <input type='text' placeholder='subject' className='input' />
             <textarea placeholder='message' className='textarea'></textarea>
             <button className='btn rounded-full border border-white/50 max-w-[170px]
-            px-8 transition-all suration-300 flex items-center justify-center overflow-hidden
+            px-8 transition-all duration-300 flex items-center justify-center overflow-hidden
             hover:border-green-700 group'>
-              <span className=' group-hover:-translate-y-[120%] group-hover:opacity-0
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0
               transition-all duration-500'>
                 Let’s talk
               </span>
