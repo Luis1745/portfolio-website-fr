4dd563022637d735a8ac9230dad95a0f3cd1b2ae
Luis1745 <luisfernando17450@gmail.com>
2024-08-19 18:42:43 -0600

    update

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className='h-full bg-primary/30'>
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
-        <div className='flex flex-col w-full max-w-[700px]'>
+        <div className='flex flex-col w-full max-w-[700px] bg-white/5 p-4 rounded-3xl'>
           <motion.h2
             variants={fadeIn('up', 0.2)}
             initial='hidden'
