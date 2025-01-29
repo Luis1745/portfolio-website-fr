e4af30c4f863bf1164dee9f1e008ba56e784fae0
Luis1745 <luisfernando17450@gmail.com>
2025-01-28 19:39:24 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,24 @@ import { useRouter } from 'next/router';
 
 import { AnimatePresence, motion } from 'framer-motion';
 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <Layout>
-      <AnimatePresence mode='wait'>
-        <motion.div key={router.route} className='h-full'>
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+    <>
+      <Head>
+        <link rel="icon" href="/log0o.png" />
+      </Head>
+      <Layout>
+        <AnimatePresence mode='wait'>
+          <motion.div key={router.route} className='h-full'>
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
